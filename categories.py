02fb8d22cd2a19f8b1bb1d63f4204a85274273f1
sanches21pm <krasnozhonov.210120045@gmail.com>
2024-11-14 13:45:12 +0500

    fix cate

--- categories.py
+++ categories.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Category, Product, ProductImage
